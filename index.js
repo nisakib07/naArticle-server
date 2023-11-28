6dd88e07f8658e243ca0ecbd47ad9a62cdcbc227
nisakib07 <websakib07@gmail.com>
2023-11-28 13:27:40 +0600

    Make Admin api done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -92,6 +92,19 @@ async function run() {
         isAdmin = user?.role === "admin";
       }
       res.send({ isAdmin });
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
