bc37363d6535a4a3da707aa630e847d5385a7308
nisakib07 <websakib07@gmail.com>
2023-11-30 14:45:57 +0600

    Single User Found

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -187,6 +187,15 @@ async function run() {
       res.send({ isAdmin });
     });
 
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+
+      const filter = { email: email };
+      const user = await userCollection.findOne(filter);
+
+      res.send(user);
+    });
+
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -198,6 +207,40 @@ async function run() {
 
       const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
+    });
+
+    app.put("/users/email", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query?.email };
+      }
+      const body = req.body;
+      const updatedDoc = {
+        $set: {
+          ...body,
+        },
+      };
+      console.log(query);
+      const options = { upsert: true };
+      const result = await userCollection.updateOne(query, updatedDoc, options);
+      res.send(result);
+    });
+
+    // payment intent
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      //   console.log(amount, "amount inside the intent");
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     await client.db("admin").command({ ping: 1 });
